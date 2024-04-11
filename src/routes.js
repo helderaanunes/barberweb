ca79f1aaecee4fc324b7b5200556271dd7d857d6
AndersonMendes2 <127905850+AndersonMendes2@users.noreply.github.com>
2024-04-10 21:52:58 -0300

    Foi Criado a Rota de tela Financeira

--- src/routes.js
+++ src/routes.js
@@ -50,9 +50,13 @@ const Toasts = React.lazy(() => import('./views/notifications/toasts/Toasts'))
 
 const Widgets = React.lazy(() => import('./views/widgets/Widgets'))
 const CategoriaAdd = React.lazy(() => import('./views/categoria/Add'))
+const FluxoFinanceiroAdd = React.lazy(() => import('./views/fluxoFinanceiro/Add'))
+const FluxoFinanceiroList = React.lazy(() => import('./views/fluxoFinanceiro/List'))
 const routes = [
   { path: '/', exact: true, name: 'Home' },
   { path: '/dashboard', name: 'Dashboard', element: Dashboard },
+  { path: '/fluxoFinanceiro/add', name: 'Adicionar FluxoFinanceiro', element: FluxoFinanceiroAdd },
+  { path: '/fluxoFinaceiro/list', name: 'Listar FluxoFinanceiro', element: FluxoFinanceiroList },
   { path: '/categoria/add', name: 'Adicionar Categoria', element: CategoriaAdd },
   { path: '/theme', name: 'Theme', element: Colors, exact: true },
   { path: '/theme/colors', name: 'Colors', element: Colors },
