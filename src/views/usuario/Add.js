2673679248f63db5ff13ceb8e614fbdf36fb10cc
ClaraVasconcelos <you@example.commcsv@discente.ifpe.edu.br>
2024-04-17 21:50:23 -0300

    Formulário Usuário

--- src/views/usuario/Add.js
+++ src/views/usuario/Add.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios'
 import {
   CButton,
   CCard,
@@ -12,34 +13,44 @@ import {
   CRow,
 } from '@coreui/react'
 import { DocsExample } from 'src/components'
+import CIcon from '@coreui/icons-react'
 
 const AddUsuario = () => {
+  const handlerEnviarFormulario = () => {
+    axios
+  }
   return (
     <CRow>
       <CCol xs={12}>
         <CCard className="mb-4">
           <CCardHeader>
-            <strong>Adicionar Usuário</strong>
+            <strong>Cadastrar Usuário</strong>
           </CCardHeader>
           <CCardBody>
-              <CForm>
-                <div className="mb-3">
-                  <CFormLabel htmlFor="nome">Nome</CFormLabel>
-                  <CFormInput
-                    type="text"
-                    id="nome"
-                    placeholder="Nome"
-                  />
-                </div>
-                <div className="mb-3">
-                  <CFormLabel htmlFor="exampleFormControlTextarea1">Example textarea</CFormLabel>
-                  <CFormTextarea id="exampleFormControlTextarea1" rows={3}></CFormTextarea>
-                </div>
-              </CForm>
+            <CForm>
+              <div className="mb-3">
+                <CFormLabel htmlFor="nome">Nome</CFormLabel>
+                <CFormInput type="text" id="nome" placeholder="Digite seu nome" />
+              </div>
+              <div className="mb-3">
+                <CFormLabel htmlFor="email">E-mail</CFormLabel>
+                <CFormInput type="email" id="email" placeholder="seuEmail@exemplo.com" />
+              </div>
+              <div className="mb-3">
+                <CFormLabel htmlFor="senha">Senha</CFormLabel>
+                <CFormInput type="password" id="senha" placeholder="Digite sua senha" />
+              </div>
+              <div className="mb-3">
+                <CFormLabel htmlFor="cargo">Cargo</CFormLabel>
+                <CFormInput type="text" id="cargo" placeholder="Digite seu cargo" />
+              </div>
+              <CButton color="success" onClick={handlerEnviarFormulario}>
+                Cadastrar
+              </CButton>
+            </CForm>
           </CCardBody>
         </CCard>
       </CCol>
-
     </CRow>
   )
 }
