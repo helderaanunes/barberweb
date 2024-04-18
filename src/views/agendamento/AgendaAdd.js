bc622c95488e86e1f9fbc88227d81d5f880ad02b
Matheus Lima <matheus2018.1997@gmail.com>
2024-04-17 21:48:31 -0300

    Criado formulário para cadastro de cliente

--- src/views/agendamento/AgendaAdd.js
+++ src/views/agendamento/AgendaAdd.js
@@ -7,7 +7,7 @@ import {
   CFormInput,
   CFormLabel,
   CFormTextarea,
-  CRow
+  CRow,
 } from '@coreui/react'
 import React from 'react'
 
@@ -20,20 +20,16 @@ const FormControl = () => {
             <strong>Agendar</strong>
           </CCardHeader>
           <CCardBody>
-              <CForm>
-                <div className="mb-3">
-                  <CFormLabel htmlFor="Nome Completo">Email address</CFormLabel>
-                  <CFormInput
-                    type="text"
-                    id="Nome Completo"
-                    placeholder="Nome Completo"
-                  />
-                </div>
-                <div className="mb-3">
-                  <CFormLabel htmlFor="exampleFormControlTextarea1">Example textarea</CFormLabel>
-                  <CFormTextarea id="exampleFormControlTextarea1" rows={3}></CFormTextarea>
-                </div>
-              </CForm>
+            <CForm>
+              <div className="mb-3">
+                <CFormLabel htmlFor="Nome Completo">Email address</CFormLabel>
+                <CFormInput type="text" id="Nome Completo" placeholder="Nome Completo" />
+              </div>
+              <div className="mb-3">
+                <CFormLabel htmlFor="exampleFormControlTextarea1">Example textarea</CFormLabel>
+                <CFormTextarea id="exampleFormControlTextarea1" rows={3}></CFormTextarea>
+              </div>
+            </CForm>
           </CCardBody>
         </CCard>
       </CCol>
