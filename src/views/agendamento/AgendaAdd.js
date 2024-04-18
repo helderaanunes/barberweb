0495f71a5b61f3a7491a6046cb6271ad423cbdb7
helderaanunes <56598689+helderaanunes@users.noreply.github.com>
2024-04-17 21:48:05 -0300

    kkkkkkkkkk

--- src/views/agendamento/AgendaAdd.js
+++ src/views/agendamento/AgendaAdd.js
@@ -1,4 +1,9 @@
+import { cilCheck } from '@coreui/icons'
+import CIcon from '@coreui/icons-react'
+import axios from "axios";
+
 import {
+  CButton,
   CCard,
   CCardBody,
   CCardHeader,
@@ -7,11 +12,19 @@ import {
   CFormInput,
   CFormLabel,
   CFormTextarea,
-  CRow
+  CRow,
 } from '@coreui/react'
 import React from 'react'
 
 const FormControl = () => {
+  const handlerEnviarFormulario = () =>{
+    axios.post('http://localhost:8080/categoria', {'nome':'Forno'})
+    .then(response => {console.log('Response:', response.data);
+    })
+    .catch(error => {console.error('Error:', error);
+    });
+  };
+
   return (
     <CRow>
       <CCol xs={12}>
@@ -20,20 +33,26 @@ const FormControl = () => {
             <strong>Agendar</strong>
           </CCardHeader>
           <CCardBody>
-              <CForm>
-                <div className="mb-3">
-                  <CFormLabel htmlFor="Nome Completo">Email address</CFormLabel>
-                  <CFormInput
-                    type="text"
-                    id="Nome Completo"
-                    placeholder="Nome Completo"
-                  />
-                </div>
-                <div className="mb-3">
-                  <CFormLabel htmlFor="exampleFormControlTextarea1">Example textarea</CFormLabel>
-                  <CFormTextarea id="exampleFormControlTextarea1" rows={3}></CFormTextarea>
-                </div>
-              </CForm>
+            <CForm>
+              <div className="mb-3">
+                <CFormLabel htmlFor="data">Data</CFormLabel>
+                <CFormInput type="date" id="data" placeholder="Data" />
+              </div>
+              <div className="mb-3">
+                <CFormLabel htmlFor="hora">Hora</CFormLabel>
+                <CFormInput type="time" id="hora" placeholder="Hora" />
+              </div>
+              <div className="mb-3">
+                <CFormLabel htmlFor="taxa">Taxa para agendamento</CFormLabel>
+                <CFormInput type="number" id="taxa" placeholder="taxa" />
+              </div>
+              <div className="mb-3">
+                <CButton color="success" onClick={handlerEnviarFormulario}>
+                  <CIcon icon={cilCheck} className="me-2" />
+                  Salvar
+                </CButton>
+              </div>
+            </CForm>
           </CCardBody>
         </CCard>
       </CCol>
