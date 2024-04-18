aabe66e1145079a1d920d306a5aa87228459337b
Márcia <marciacriss007@gmail.com>
2024-04-17 21:48:35 -0300

    venda

--- src/views/venda/Add.js
+++ src/views/venda/Add.js
@@ -12,8 +12,16 @@ import {
   CRow,
 } from '@coreui/react'
 import { DocsExample } from 'src/components'
+import CIcon from '@coreui/icons-react'
+import { cilCheck } from '@coreui/icons'
+import axios from "axios"
 
 const FormControl = () => {
+  const handlerEnviarFormulario = () =>{
+    axios.post('',{})
+
+  };
+
   return (
     <CRow>
       <CCol xs={12}>
@@ -22,16 +30,26 @@ const FormControl = () => {
             <strong>Cadastrar Venda</strong>
           </CCardHeader>
           <CCardBody>
-              <CForm>
-                <div className="mb-3">
-                  <CFormLabel htmlFor="Desconto">Desconto</CFormLabel>
-                  <CFormInput
-                    type="namber"
-                    id="desconto"
-                    placeholder="Desconto"
-                  />
-                </div>
-              </CForm>
+            <CForm>
+              <div className="mb-3">
+                <CFormLabel htmlFor="Desconto">Desconto</CFormLabel>
+                <CFormInput type="number" id="desconto" placeholder="Desconto" />
+              </div>
+              <div className="mb-3">
+                <CFormLabel htmlFor="data">Data</CFormLabel>
+                <CFormInput type="date" id="data" placeholder="Data" />
+              </div>
+              <div className="mb-3">
+                <CFormLabel htmlFor="hora">Hora</CFormLabel>
+                <CFormInput type="time" id="hora" placeholder="Hora" />
+              </div>
+              <div className="mb-3">
+                <CButton color="success" onClick={}>
+                  <CIcon icon={cilCheck} className="me-2"/>
+                  Salvar
+                </CButton>
+              </div>
+            </CForm>
           </CCardBody>
         </CCard>
       </CCol>
